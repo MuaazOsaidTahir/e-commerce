f65efcf3c372e0f3b072c980386a25bc1d1e255f
Muaaz Osaid Tahir <66464591+MuaazOsaidTahir@users.noreply.github.com>
2023-08-08 00:57:43 +0500

    cleaning up

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Muaaz:e5%40CVRyz3JFAyRi@cluster0.u7wq2.mongodb.net/ecommercestore?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("MONGODB_URI", { useNewUrlParser: true, useUnifiedTopology: true })
 
-// mongoose.connect("mongodb+srv://Muaaz:e5@CVRyz3JFAyRi@cluster0.zkhtc.mongodb.net/FullStackApp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
